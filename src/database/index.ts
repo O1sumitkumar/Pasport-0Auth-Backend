391f941a65840c1e70bdb0db6653813b3494c7fa
O1sumitkumar <SUMITJHA365@gmail.com>
2023-12-28 09:11:27 +0530

    update version

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,7 +5,7 @@ export const dbConnection = async () => {
   const dbConfig = {
     url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
     options: {
-      useNewUrlParser: true,
+      // useNewUrlParser: true,
       useUnifiedTopology: true,
     },
   };
@@ -14,5 +14,5 @@ export const dbConnection = async () => {
     set('debug', true);
   }
 
-  await connect(dbConfig.url, dbConfig.options);
+  await connect(dbConfig.url);
 };
